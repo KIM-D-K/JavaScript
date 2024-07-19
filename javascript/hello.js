4411100e22fcc64e6f4306e0cb70cf1852d27413
JavavaHHH <105289238+KIM-D-K@users.noreply.github.com>
2024-07-19 14:45:27 +0900

    Add files via upload

---
+++ javascript/hello.js
@@ -0,0 +1,13 @@
+// 경고창을 띄움
+// window.alert("오늘 자바스크립트 배우는 날")
+
+// window.console.log("주말에는 놀라갈까?");
+
+// 주말에 계획 : 미친  자격증 공부
+// 오늘 저녁 메뉴 : ㅁㄹ
+// 자바스크립트 후기 : 다까먹음
+
+// window.console.log("주말에 계획 : 미친  자격증 공부")
+// window.console.log("오늘 저녁 메뉴 : ㅁㄹ")
+// window.console.log("자바스크립트 후기 : 다까먹음")
+
---
+++ javascript/memory/index.js
@@ -0,0 +1,25 @@
+const name = "kim"
+const coffee = "coffee"
+
+// window.console.log(name + coffee)
+// window.console.log("제 이름은 name 좋아하는 커피는 coffee 입니다.")
+// window.console.log(`제 이름은 ${name} 좋아하는 커피는 ${coffee} 입니다.`)// ` `빽틱
+
+// mbti, weekend, youtube
+// 제 mbti는 ? , 주말에 ? 하구요, 유튜브는 ? 를 볼거에요!?
+
+// const mbti = "ENTJ"
+// const week = "공부"
+// const youtube = "안봐"
+
+// window.console.log(`제 MBTI는 ${mbti}입니다. 주말에는 ${week} 하고, 유튜브는 ${youtube}요.ㅋ`)
+
+// 프롬프트로 당신의 핸드폰 기종은?
+// 프롬프트로 당신의 PC 기종은?
+
+// 콘솔로 당신이 쓰는 핸드폰은 ~~이고, 당신의 PC는 ~~이군요!
+
+const phone = window.prompt("당신의 핸드폰 기종은?")
+const pc = window.prompt("당신의 pc 기종은?")
+
+window.console.log(`당신의 쓰는 핸드폰은 ${phone}이고, 당신의 pc는 ${pc}이군요?!`)
