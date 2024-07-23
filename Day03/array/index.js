3935cc6db4bdbef7faece30d518e028ee816aa31
JavavaHHH <105289238+KIM-D-K@users.noreply.github.com>
2024-07-23 14:47:43 +0900

    Add files via upload

---
+++ Day03/array/index.js
@@ -0,0 +1,12 @@
+// array[배열]
+
+const ages = [1, 2, 3, 4, 5, 6, 7]
+const fruits = ["tomato", "peach", "strawberry", "grape"]
+
+console.log(ages[3]) // 4
+console.log(fruits[0]) // tomato
+
+const megacoffee = [["아아", "라떼", "연유라떼"], ["초코쿠키", "마카롱", "타르트"]] // 2차원 배열
+
+console.log(megacoffee[0][1]) // 라떼가 출력
+console.log(megacoffee[1][2]) // 타르트가 출력
---
+++ Day03/review/index.js
@@ -0,0 +1,12 @@
+
+
+const lunch = prompt("점심 메뉴?")
+console.log(`${lunch} 맛있겠네여`)
+
+const width = Number(prompt("밑변?"))
+const height = Number(prompt("높이?"))
+console.log(`사각형의 넓이는 ${Number(width)*Number(height)}입니다.`)
+
+const won = Number(prompt("엔화로 바꿔주는 시스템 입니다."))
+const en = won/8.85
+console.log(`${won}원 -> ${en}엔 입니다.`)
